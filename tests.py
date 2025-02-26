47ab1edc41b3533db3bc17f0f46ba47cbcf4017a
gijstemp <gijstempelman@gmail.com>
2025-02-26 10:16:58 +0000

    finalize code and unittests

--- tests.py
+++ tests.py
@@ -5,8 +5,7 @@ class Tests(unittest.TestCase):
     def test_maze_create_cells(self):
         num_cols = 12
         num_rows = 10
-        window = Window(800, 600)  # Create a Window object
-        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, window)
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(
             len(m1._cells),
             num_cols,
@@ -15,7 +14,7 @@ class Tests(unittest.TestCase):
             len(m1._cells[0]),
             num_rows,
         )
-    
+
     def test_maze_break_entrance_and_exit(self):
         num_cols = 12
         num_rows = 10
@@ -28,6 +27,17 @@ class Tests(unittest.TestCase):
             m1._cells[num_cols - 1][num_rows - 1].has_bottom_wall,
             False,
         )
+
+    def test_maze_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for col in m1._cells:
+            for cell in col:
+                self.assertEqual(
+                    cell.visited,
+                    False,
+                )
 
 if __name__ == "__main__":
     unittest.main()
